4e3b7d241eccce557f9d999a836b11c251ef28f0
L <luenwarneke@gmail.com>
2024-10-23 11:51:15 +1000

    vercel.json

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,3 @@
-// next.config.js
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config: { externals: string[] | { [key: string]: string }; }, { isServer }: { isServer: boolean }) => {
