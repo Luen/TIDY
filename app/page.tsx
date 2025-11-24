4cc176111c66d7f3dbf2c31c2931e0e397be0402
luen <Luen@users.noreply.github.com>
2025-11-24 10:35:36 +1000

    upgrade tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
         <div className="text-center space-y-6 p-6 bg-gray-50 rounded-lg shadow-md max-w-lg mx-auto">
           <h2 className="text-2xl font-semibold text-gray-800 mb-4">Related Links</h2>
           <ul className="space-y-6">
-            <li className="bg-white p-4 rounded-md shadow flex flex-col items-start">
+            <li className="bg-white p-4 rounded-md shadow-sm flex flex-col items-start">
               <Link href="https://www.abc.net.au/news/2020-06-05/test-illegal-dumping-community/12042764" className="text-blue-600 hover:text-blue-800 transition-colors flex items-center space-x-3">
                 <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-blue-600" viewBox="0 0 20 20" fill="currentColor">
                   <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2 2a1 1 0 001.414-1.414L11 10.586V6z" clipRule="evenodd" />
@@ -49,7 +49,7 @@ export default function Home() {
               </Link>
               <span className="text-sm text-gray-500 mt-2">ABC News</span>
             </li>
-            <li className="bg-white p-4 rounded-md shadow flex flex-col items-start">
+            <li className="bg-white p-4 rounded-md shadow-sm flex flex-col items-start">
               <Link href="https://www.facebook.com/watch/?v=2811958278925996" className="text-blue-600 hover:text-blue-800 transition-colors flex items-center space-x-3">
                 <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-blue-600" viewBox="0 0 20 20" fill="currentColor">
                   <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2 2a1 1 0 001.414-1.414L11 10.586V6z" clipRule="evenodd" />
@@ -58,7 +58,7 @@ export default function Home() {
               </Link>
               <span className="text-sm text-gray-500 mt-2">ABC North Queensland</span>
             </li>
-            <li className="bg-white p-4 rounded-md shadow flex flex-col items-start">
+            <li className="bg-white p-4 rounded-md shadow-sm flex flex-col items-start">
               <Link href="https://climatesafety.info/thesustainablehour501/" className="text-blue-600 hover:text-blue-800 transition-colors flex items-center space-x-3">
                 <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-blue-600" viewBox="0 0 20 20" fill="currentColor">
                   <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2 2a1 1 0 001.414-1.414L11 10.586V6z" clipRule="evenodd" />
@@ -67,7 +67,7 @@ export default function Home() {
               </Link>
               <span className="text-sm text-gray-500 mt-2">Centre for Climate Safety</span>
             </li>
-            <li className="bg-white p-4 rounded-md shadow flex flex-col items-start">
+            <li className="bg-white p-4 rounded-md shadow-sm flex flex-col items-start">
               <Link href="https://www.townsville.qld.gov.au/about-council/news-and-publications/media-releases/2023/april/council-installs-gates-boulders-at-illegal-dumping-hot-spot" className="text-blue-600 hover:text-blue-800 transition-colors flex items-center space-x-3">
                 <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-blue-600" viewBox="0 0 20 20" fill="currentColor">
                   <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2 2a1 1 0 001.414-1.414L11 10.586V6z" clipRule="evenodd" />
@@ -75,7 +75,7 @@ export default function Home() {
                 <span className="text-lg font-medium">Council installs gates, boulders at illegal dumping hot spot</span>
               </Link>
               <span className="text-sm text-gray-500 mt-2">Townsville City Council</span>
-            </li><li className="bg-white p-4 rounded-md shadow flex flex-col items-start">
+            </li><li className="bg-white p-4 rounded-md shadow-sm flex flex-col items-start">
               <Link href="https://www.townsvillebulletin.com.au/news/townsville/tidys-dave-dudley-wants-government-action-on-townsvilles-illegal-dumping/news-story/fb2530ab66949edf21679f6abfe5b756" className="text-blue-600 hover:text-blue-800 transition-colors flex items-center space-x-3">
                 <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-blue-600" viewBox="0 0 20 20" fill="currentColor">
                   <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2 2a1 1 0 001.414-1.414L11 10.586V6z" clipRule="evenodd" />
@@ -84,7 +84,7 @@ export default function Home() {
               </Link>
               <span className="text-sm text-gray-500 mt-2">Townsville Bulletin</span>
             </li>
-            <li className="bg-white p-4 rounded-md shadow flex flex-col items-start">
+            <li className="bg-white p-4 rounded-md shadow-sm flex flex-col items-start">
               <Link href="https://www.drytropicshealthywaters.org/post/tidy-up-townsville-saving-a-shaw-rd-reserve" className="text-blue-600 hover:text-blue-800 transition-colors flex items-center space-x-3">
                 <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-blue-600" viewBox="0 0 20 20" fill="currentColor">
                   <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2 2a1 1 0 001.414-1.414L11 10.586V6z" clipRule="evenodd" />
@@ -93,7 +93,7 @@ export default function Home() {
               </Link>
               <span className="text-sm text-gray-500 mt-2">Healthy Waters Partnership for the Dry Tropics</span>
             </li>
-            <li className="bg-white p-4 rounded-md shadow flex flex-col items-start">
+            <li className="bg-white p-4 rounded-md shadow-sm flex flex-col items-start">
               <Link href="https://wanderstories.space/leave-no-trace/" className="text-blue-600 hover:text-blue-800 transition-colors flex items-center space-x-3">
                 <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-blue-600" viewBox="0 0 20 20" fill="currentColor">
                   <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2 2a1 1 0 001.414-1.414L11 10.586V6z" clipRule="evenodd" />
